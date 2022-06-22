964bfd1ec7ba7493b2b806ee312f2b2750b2d633
Brian Swedberg <bwswedberg@gmail.com>
2022-06-22 07:04:44 -0400

    updaed style

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,9 +7,9 @@ export const App: FC = () => {
       <div className="app-description">
         <h1 className="app-title">Geolife Demo</h1>
         <nav className="app-nav">
-          <Link className="app-nav_link" to="/mbtiles">MBTiles Source (17m)</Link>
-          <Link className="app-nav_link" to="/geojson?limit=1000000">GeoJSON Source (1m)</Link>
-          <Link className="app-nav_link" to="/geojson?limit=500000">GeoJSON Source (0.5m)</Link>
+          <Link className="app-nav_link" to="/mbtiles">MBTiles (17m)</Link>
+          <Link className="app-nav_link" to="/geojson?limit=1000000">GeoJSON (1m)</Link>
+          <Link className="app-nav_link" to="/geojson?limit=500000">GeoJSON (0.5m)</Link>
         </nav>
       </div>
       <Outlet />
